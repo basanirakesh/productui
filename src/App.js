6ea87dc93991946ceee142e45b39457bc66adc4e
Naveen <you@example.com>
2020-08-07 20:20:28 +0530

    Added second screen component to the App component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from "./component/Header";
 import ProductCatalog from './component/ProductCatalogComponents/ProductCatalog';
+import ProductDetails from './component/ProductCatalogComponents/ProductDetails';
 
 function App() {
     return (
         <div>
             <Header/>
             <ProductCatalog/>
+            <br /><br /><br />
+            <ProductDetails />
         </div>
     );
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,7 @@ import React from "react";
 function Header() {
     return (
         <div className="container">
-            {}
+            <h2>ING Products catalog</h2>
         </div>
     );
 }
