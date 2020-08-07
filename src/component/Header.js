5627d987eb6f8cccbf0b329b6646dbcd8ea268f1
Dmitry Lepskiy <lepsky.dmitry@gmail.com>
2020-08-07 16:07:15 +0200

    + proxying api requests to real api

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,18 +1,11 @@
 //import "./Header.css";
-import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchUserProducts, selectProducts} from "../features/products";
+import React from "react";
 
 
 function Header() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchUserProducts());
-    }, [dispatch])
-    const {loading, entities} = useSelector(selectProducts);
     return (
         <div className="container">
-            {`items loaded: ${JSON.stringify(entities)}`}
+            {}
         </div>
     );
 }
--- src/component/ProductCatalogComponents/ProductCatalog.js
+++ src/component/ProductCatalogComponents/ProductCatalog.js
@@ -1,72 +1,22 @@
 //import "./ProductCatalog.css";
-import React, { Component } from "react";
-import ProductGroup from "./ProductGroup";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchUserProducts, selectProducts} from "../../features/products";
 
-class ProductCatalog extends Component {
+function ProductCatalog() {
 
-    constructor(props) {
-        super(props);
-        //this.constants = new Constants();
-        this.state = {
-            productList: [],
-            catalogList: {}
-        };
-    }
-
-    componentDidMount() {
-        this.processCatalogResponse(this.sampleCatalog());
-    }
-
-    processCatalogResponse = (catalogResponse) => {
-        catalogResponse.forEach((v, i, arr) => {
-            if (v.group in this.state.catalogList) {
-                let catalogList = this.state.catalogList;
-                catalogList[v.group].push(v);
-                this.setState({"catalogList": catalogList});
-            } else {
-                catalogList[v.group] = [];
-                catalogList[v.group].push(v);
-                this.setState({"catalogList": catalogList});
-            }
-        });
-    }
-
-    sampleCatalog = () => {
-        return [
-            {
-              "name": "Orange savings account",
-              "group": "Saving",
-              "id": "12"
-            },
-            {
-              "name": "Bonus interest account",
-              "group": "Saving",
-              "id": "13"
-            },
-            {
-              "name": "Interest only",
-              "group": "Mortgage",
-              "id": "14"
-            }
-          ];
-    }
-
-  render() {
-      let productGroupKV = [];
-      Object.keys(catalogList).forEach((v, i, arr) => {
-        productGroupKV.push({key: v, value: this.catalogList[v]});
-      });
-      productGroup = this.productGroupKV.map((entry, index) => {
-        return productGroup.push(<li><br />{entry.group}<br /><ProductGroup entry={entry}/></li>);
-      });
-        return (
-            <div className="container">
-                <ul>
-                    {productGroup}
-                </ul>
-            </div>
-        );
-      }
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchUserProducts());
+    }, [dispatch])
+    const {loading, entities} = useSelector(selectProducts);
+    return (
+        <div className="container">
+            <ul>
+                {entities.map(product => <li>{product.name}</li>)}
+            </ul>
+        </div>
+    );
 }
 
 export default ProductCatalog;
