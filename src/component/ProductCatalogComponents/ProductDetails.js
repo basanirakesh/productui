4150f03f3c503608f32273b6ef3567100821dc1f
Naveen <you@example.com>
2020-08-07 20:03:30 +0530

    Added new component for product details

---
+++ src/component/ProductCatalogComponents/ProductDetails.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ProductCatalog from './ProductCatalog';
+
+function ProductDetails() {
+  return (
+    <div className="container">
+       <br />
+       <div>
+         <h3>Product Details</h3>
+         <br />
+         <hr />
+         <h4>Selected Product Group</h4>
+         <br />
+         Selected product group name
+         <hr />
+         aaa: abc
+         aaa: abc%
+         aaa: abc
+         aaa: abc%
+         <div>
+           <h4>Other Group products</h4>
+         </div>
+       </div>
+    </div>
+  );
+}
+
+export default ProductDetails;
