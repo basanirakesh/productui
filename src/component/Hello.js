f41b5f5baacc08834a7658cd82264696d489f33c
Naveen <you@example.com>
2020-08-07 17:25:35 +0530

    Initial Component structure

---
+++ src/component/Hello.js
@@ -0,0 +1,22 @@
+//import "./Hello.css";
+import React, { Component } from "react";
+
+class Hello extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+  }
+  render() {
+    return (
+      <div className="container">
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default Hello;
