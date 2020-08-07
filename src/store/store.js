d62aee497e566c9da9fa215f75d5a229976bda18
Dmitry Lepskiy <lepsky.dmitry@gmail.com>
2020-08-07 17:43:39 +0200

    - unused slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import {productsReducer} from '../features/products/index';
 
 import logger from 'redux-logger'
@@ -8,7 +7,6 @@ const middleware = [...getDefaultMiddleware(), logger]
 
 export default configureStore({
     reducer: {
-        counter: counterReducer,
         products: productsReducer
     },
     middleware
