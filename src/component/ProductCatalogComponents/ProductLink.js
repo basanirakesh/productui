9b8aae63440c4e342b68b6bb80a7d6e43419bd8c
Dmitry Lepskiy <lepsky.dmitry@gmail.com>
2020-08-07 17:32:16 +0200

    + dummy link to product details page

--- src/component/ProductCatalogComponents/ProductLink.js
+++ src/component/ProductCatalogComponents/ProductLink.js
@@ -1,9 +1,10 @@
 import React from "react";
 
+//TODO use react-router's Link component
 function ProductLink({product}) {
     return (
         <div>
-            <li key={product.id}>{product.name}</li>
+            <li key={product.id}><a href={`/products/${product.id}`}>{product.name}</a></li>
         </div>
     );
 }
